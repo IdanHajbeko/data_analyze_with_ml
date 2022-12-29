a752598a395396d27cdf6409cb027c1a2552e39d
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2022-12-29 15:45:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+#import liberys
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 
-music_data = pd.read_csv('music.csv')
-x = music_data.drop(columns=['genre'])
-y = music_data[['genre']]
+#with pandas we read the .csv file
+Personality_data = pd.read_csv('psyc.csv')
 
+#with pandas we clean and preper the data for use
+x = Personality_data.drop(columns=['Personality'])
+y = Personality_data[["Personality"]]
+
+#we use the the model DecisionTreeClassifier from the sklearn.tree to make a machine learning model with the data from thhe .csv file
 model = DecisionTreeClassifier()
 model.fit(x, y)
 
-#model = joblib.load('music-recommender.joblib')
-age = float(input("what is your age: "))
-gender = float(input("are you a man or a woman 1 for man 0 for woman: "))
-prediction = model.predict([[age, gender]])
+
+#we save the model in a .joblib file
+model = joblib.dump('Personality_teller.joblib')
+#now we can just load the model from the .joblib file and dont need steps 1, 2, 3, and 4
+#model = joblib.load('Personality_teller.joblib')
+
+age = float(input("what you age: "))
+openness = float(input("how open you are: "))
+neuroticism = float(input("neuroticism: "))
+conscientiousness = float(input("conscientiousness: "))
+agreeableness = float(input("agreeableness: "))
+extraversion = float(input("extraversion: "))
+
+#we make a prediction with the machine learning model
+prediction = model.predict([[age,openness,neuroticism,conscientiousness,agreeableness,extraversion]])
 print(prediction)
